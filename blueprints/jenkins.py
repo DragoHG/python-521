5dac195bae73f95fb65123096b5dc70e73452559
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-06-01 17:10:44 -0300

    adicionado funcao para editar xml do job do jenkins

--- blueprints/jenkins.py
+++ blueprints/jenkins.py
@@ -12,38 +12,42 @@ connection = jenkins.Jenkins(URL, USER, PASSWD)
 
 @blueprint.route('/jenkins', methods=[ 'GET' ])
 def get_jenkins():
-    
-    context = {
-        'page': 'jenkins',
-        'route': {
-            'is_public': False
-        },
-        'jobs': [
-            connection.get_job_info(j['name'])
-                for j in connection.get_jobs()
-        ]
-    }
+	
+	context = {
+		'page': 'jenkins',
+		'route': {
+			'is_public': False
+		},
+		'jobs': [
+			connection.get_job_info(j['name'])
+				for j in connection.get_jobs()
+		]
+	}
 
-    return flask.render_template('jenkins.html', context=context)
+	return flask.render_template('jenkins.html', context=context)
 
 @blueprint.route('/jenkins', methods=[ 'GET' ])
 def post_jenkins():
-    pass
+	pass
 
 @blueprint.route('/jenkins/update/<string:jobname>', methods=[ 'GET' ])
 def get_jenkins_update(jobname):
-    
-    context = {
-        'page': 'jenkins-update',
-        'route': {
-            'is_public': False
-        },
-    }
+	
+	context = {
+		'page': 'jenkins-update',
+		'route': {
+			'is_public': False
+		},
+		'job': {
+		'name': jobname,
+		'xml': connection.get_job_config(jobname)
+		}
+	}
 
-    return flask.render_template('jenkins_update.html', context=context)
+	return flask.render_template('jenkins_update.html', context=context)
 
 @blueprint.route('/jenkins/build/<string:jobname>', methods=[ 'GET' ])
 def get_jenkins_build(jobname):
-    connection.build_job(jobname)
+	connection.build_job(jobname)
 
-    return flask.redirect('/jenkins')
+	return flask.redirect('/jenkins')
